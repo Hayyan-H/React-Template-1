4c8356e3dafa880658d5ae902a60ef92e98f1b91
Hayyan <hayyan.hamchou@gmail.com>
2025-09-24 21:19:00 +0200

    moved the studio-template-1 folder out

--- studio-template-1/schemaTypes/index.ts
+++
@@ -1,7 +0,0 @@
-import {general} from './general'
-import {homepage} from './homepage'
-import {FAQs} from './FAQs'
-import {about} from './about'
-import {contact} from './contact'
-
-export const schemaTypes = [general, homepage, FAQs, about, contact]
